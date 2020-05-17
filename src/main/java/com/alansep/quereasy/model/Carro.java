0c8f30b44cd24df862c35fa5aa18e9a702c7ec9f
Gabriel Alan <alang.von@gmail.com>
2020-05-17 16:00:45 -0300

    Testes

---
+++ src/main/java/com/alansep/quereasy/model/Carro.java
@@ -0,0 +1,28 @@
+package com.alansep.quereasy.model;
+
+public class Carro {
+
+	private String modelo;
+	private Integer ano;
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public Integer getAno() {
+		return ano;
+	}
+
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+	
+	public void buzinar() {
+		System.out.println("zzzzzzzzzzzzzzz!!");
+	}
+	
+}
--- src/main/java/com/alansep/quereasy/testes/Principal.java
+++ src/main/java/com/alansep/quereasy/testes/Principal.java
@@ -1,13 +1,35 @@
 package com.alansep.quereasy.testes;
 
-import com.alansep.quereasy.Quereasy;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import com.alansep.quereasy.model.Carro;
 
 public class Principal {
 
 	public static void main(String[] args) {
 
-		System.out.println(new Quereasy().isNumber(null));
+		// System.out.println(new Quereasy().isNumber(null));
 
+		Carro carro = new Carro();
+
+		// System.out.println(numero);
+
+		Class<?> klass = carro.getClass();
+
+		Method[] metodos = klass.getMethods();
+		
+		Field[] atributos = klass.getDeclaredFields();
+		
+		for (int i = 0; i < metodos.length; i++) {
+		//	System.out.println(metodos[i].getName() + " - " + metodos[i].getReturnType());
+		}
+		
+		for (int i = 0; i < atributos.length; i++) {
+			System.out.println(atributos[i].getType().getSimpleName() + " - " + atributos[i].getName());
+		}
+		
+	
+	
 	}
-
 }
