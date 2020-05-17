502376a087289b5b96204b0cf5602935af8a01f5
Gabriel Alan <alang.von@gmail.com>
2020-05-17 16:32:36 -0300

    Anotação de Tabela.

--- src/main/java/com/alansep/quereasy/Quereasy.java
+++ src/main/java/com/alansep/quereasy/Quereasy.java
@@ -1,34 +1,35 @@
 package com.alansep.quereasy;
 
+import com.alansep.quereasy.anotations.Tabela;
 import com.alansep.quereasy.generators.GeradorCreateTable;
 
 public class Quereasy implements GeradorCreateTable {
 
 	@Override
 	public String gerarQuery(Object object) {
-		String query = "CREATE TABLE " + gerarNomeTabela(object);
-		
+		String query = "CREATE TABLE " + gerarNomeTabela(object) + " ( ";
+
 		/*
-		 * CREATE TABLE nome_tabela (
-		 * modelo varchar(20),
-		 * ano integer );
+		 * CREATE TABLE nome_tabela ( modelo varchar(20), ano integer );
 		 * 
 		 * 
 		 * 
 		 * 
 		 * 
-		 * */
-		
-		
+		 */
+
 		// TODO Auto-generated method stub
 		return query;
 	}
 
 	private String gerarNomeTabela(Object object) {
-		return Util.pegarClasse(object).getSimpleName();
+		Class<?> classe = Util.pegarClasse(object);
+		if (classe.isAnnotationPresent(Tabela.class)) {
+			Tabela annotation = classe.getAnnotation(Tabela.class);
+			return annotation.nome();
+		} else {
+			return classe.getSimpleName();
+		}
 	}
-
-
-	
 
 }
